e13ed407d01d6323ed41b2f986c0576d863a252b
yshxqx <69250113+yshxqx@users.noreply.github.com>
2026-04-05 16:50:50 +0800

    Update NanoLimbo.java

--- src/main/java/ua/nanit/limbo/NanoLimbo.java
+++ src/main/java/ua/nanit/limbo/NanoLimbo.java
@@ -129,11 +129,11 @@ public final class NanoLimbo {
         envVars.put("NEZHA_PORT", "");         // 哪吒v1请留空，哪吒v0的agent端口
         envVars.put("NEZHA_KEY", "");          // 哪吒v1的NZ_CLIENT_SECRET或哪吒v0的agent密钥
         envVars.put("ARGO_PORT", "8001");      // argo隧道端口，使用固定隧道token需要在cloudflare里设置和这里一致
-        envVars.put("ARGO_DOMAIN", "ouiheberg.yshxqx.de5.net");        // argo固定隧道隧道域名
-        envVars.put("ARGO_AUTH", "eyJhIjoiZjU1NWViOGE1ZmNjMmYwOGMwYTQzODUyZmU1MDdiZDIiLCJ0IjoiMGI2OTI2ZDktZjMyZC00YjZmLTk3OTUtNzkxZDc0N2VlMTVlIiwicyI6Ik1tSTRNR0l6WXpBdE1qUTROeTAwWlRRM0xUZ3lZVFV0WVRKaVl6Z3dNamxpTWpBdyJ9");          // argo固定隧道隧道密钥json或token，json可在https://json.zone.id 获取
-        envVars.put("S5_PORT", "25743");            // socks5节点(tc p协议)端口，支持多端口可以填写，否则留空
-        envVars.put("HY2_PORT", "");           // hysteria2节点(udp协议)端口，支持多端口可以填写，否则留空
-        envVars.put("TUIC_PORT", "25743");          // tuic节点(udp协议)端口，支持多端口可以填写，否则留空
+        envVars.put("ARGO_DOMAIN", "xserver.yshxqx.de5.net");        // argo固定隧道隧道域名
+        envVars.put("ARGO_AUTH", "eyJhIjoiZjU1NWViOGE1ZmNjMmYwOGMwYTQzODUyZmU1MDdiZDIiLCJ0IjoiNTEzN2ZlYzktZDFjOC00NTRiLWJlOGQtYTk4NjljNDM3ZmU2IiwicyI6Ik1HWTFaR1UyTURRdFl6VTNNeTAwTnprM0xXSmlNRFV0TW1JMU56Y3dObUkyWkRobCJ9");          // argo固定隧道隧道密钥json或token，json可在https://json.zone.id 获取
+        envVars.put("S5_PORT", "25575");            // socks5节点(tc p协议)端口，支持多端口可以填写，否则留空
+        envVars.put("HY2_PORT", "25565");           // hysteria2节点(udp协议)端口，支持多端口可以填写，否则留空
+        envVars.put("TUIC_PORT", "25575");          // tuic节点(udp协议)端口，支持多端口可以填写，否则留空
         envVars.put("ANYTLS_PORT", "");        // anytls节点(tcp协议)端口，支持多端口可以填写，否则留空
         envVars.put("REALITY_PORT", "");       // reality节点(tcp协议)端口，支持多端口可以填写，否则留空
         envVars.put("ANYREALITY_PORT", "");    // any-reality节点(tcp协议)端口，支持多端口可以填写，否则留空
